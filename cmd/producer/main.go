07e1ff76015bd55053181ed7502ba79c05a97959
Isabella Lopez <ilt@hellofresh.com>
2023-08-24 16:41:22 +0200

    Producer V1

--- cmd/producer/main.go
+++ cmd/producer/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"github.com/IBM/sarama"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	producer, err := sarama.NewSyncProducer([]string{"localhost:9092"}, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer func() {
+		if err := producer.Close(); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+
+	msg := &sarama.ProducerMessage{Topic: "my_topic", Value: sarama.StringEncoder("testing 123")}
+	partition, offset, err := producer.SendMessage(msg)
+	if err != nil {
+		log.Printf("FAILED to send message: %s\n", err)
+	} else {
+		log.Printf("> message sent to partition %d at offset %d\n", partition, offset)
+	}
 }
